cb028e566d48f81ad2ef2e52053306dee8b85f92
Carlos <clm987@gmail.com>
2023-04-06 18:07:07 -0300

    routing de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: './components/bienvenido/bienvenido.component', component: BienvenidoComponent },
+  { path: './components/error/error.component', component: ErrorComponent },
+  { path: './components/login/login.component', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
